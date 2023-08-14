4c3dd949a85fa705381298aef2d62de12ac352d8
Tian707 <122786950+Tian707@users.noreply.github.com>
2023-08-14 17:08:21 +0200

    Add files via upload

---
+++ 0813ChristmasTree/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _0813ChristmasTree
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a height for your digital Christmas tree:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            for (int i = 1; i <= height; i++)
+            {
+                // For loop to print spaces
+                for (int j = 0; j < height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // For loop to print stars
+                for (int j = 0; j < 2 * i - 1; j++)
+                {
+                    // Define condition for red color
+                    if(i % 2 == 0 &&  j % 2 == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    
+                    Console.Write("*");
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
